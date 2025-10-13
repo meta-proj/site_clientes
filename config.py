628c925f450b23cce5634c55d01c074d52693ecb
meta-proj <projetos@metassessoria.com>
2025-10-13 15:49:01 -0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,26 @@
+# config.py
+
+# --- CONFIGURAÇÕES DO BANCO DE DADOS ---
+DB_CONFIG = {
+    "host": "mysql.metassessoria.com",
+    "port": 3306,
+    "user": "metassessoria",
+    "password": "Meta67ceXVb76",
+    "database": "metassessoria"
+}
+
+# --- NOMES DE RELATÓRIOS ---
+REPORTS = {
+    "dp": {
+        "name": "Departamento Pessoal"
+    },
+    "fiscal": {
+        "name": "Fiscal"
+    },
+    "contabil": {
+        "name": "Contábil"
+    },
+    "produtos": {
+        "name": "Produtos"
+    }
+}
